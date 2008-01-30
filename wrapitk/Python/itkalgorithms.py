46b86b9124c0b9db8f1dc71c101b191edee3f17b
cpbotha <cpbotha@localhost>
2008-01-30 16:51:26 +0000

    Deleted old projects from devide repo.

--- wrapitk/Python/itkalgorithms.py
+++
@@ -1,5 +0,0 @@
-from itkbasicfilters import *
-from itknumerics import *
-__itk_import_data__ = itkbase.preimport()
-from ITKAlgorithmsPython import *
-itkbase.postimport(__itk_import_data__)
--- wrapitk/Python/itkcommon.py
+++
@@ -1,4 +0,0 @@
-from vxlnumerics import *
-__itk_import_data__ = itkbase.preimport()
-from ITKCommonPython import *
-itkbase.postimport(__itk_import_data__)
