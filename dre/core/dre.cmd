7a745ef55f1da4749f2a4b89a52157a2eeadfd3e
cpbotha <cpbotha@localhost>
2009-07-29 16:41:41 +0000

    Need to add quotes around paths.  If you don't have these, installation dirs with spaces in them cause havoc.

--- dre/core/dre.cmd
+++ dre/core/dre.cmd
@@ -9,5 +9,5 @@
 @rem this script lives in the top-level DRE directory, i.e. the 'inst'
 @rem directory of the johannes build tree.
 
-%~dp0\python\python.exe %~dp0\dre.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%~dp0\python\python.exe" "%~dp0\dre.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
 
